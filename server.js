9b7d63f4304ac709db16f2704d5b7e5f9d822ea7
MelissaHub <Melissadevelopment2022@gmail.com>
2022-07-27 21:47:39 -0400

    tip caluculator

--- server.js
+++ server.js
@@ -2,8 +2,16 @@ const express = require('express')
 require('dotenv').config()
 //enviormental var
 const app = express()
+const total = 0
+const tipPercentage = 0
+
+app.get('/tip', (req, res) => {
+    res.send(total)
+    res.send(tipPercentage)
+})
 
 
+//one should be at the bottom first?
 app.get('/greeting/:name', (req, res) => {
     res.send(param)
     console.log('Hey, stranger')
